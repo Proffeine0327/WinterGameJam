7cbe4141739e643832e47d12c621b3a7b4654bcd
proffeine0327 <proffeine0327@gmail.com>
2023-01-18 19:15:11 +0900

    플레이어 기본 움직임, 머리 흔들림, 픽셀 아트화

---
+++ EscapeFrom/Assets/김웅섭/Scripts/MonoBehaviour/Player.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private static Player player;
+
+    [Header("Move")]
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpScale;
+    [SerializeField] private float gravityMultiplier;
+    [Header("Mouse")]
+    [SerializeField] private Vector2 mouseSensivity;
+    [Header("HeadHob")]
+    [SerializeField] private bool headHobEnabled;
+    [SerializeField, Range(0, 0.1f)] private float headHobAmplitude;
+    [SerializeField, Range(0, 30)] private float headHobFrequency;
+    [SerializeField] private Transform _camera;
+    [SerializeField] private Transform cameraHolder;
+
+    private CharacterController cc;
+    private float YVelocity;
+    private float MouseAngleY;
+    private float headHobToggleSpeed = 3.0f;
+    private Vector3 headStartPos;
+
+    private void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        player = this;
+
+        cc = GetComponent<CharacterController>();
+
+        headStartPos = _camera.localPosition;
+    }
+
+    private void Update()
+    {
+        HeadHob();
+        CameraMove();
+        Move();
+    }
+
+    private void HeadHob()
+    {
+        if(!headHobEnabled) return;
+
+        CheckHeadHobMotion();
+        ResetHeadPosition();
+        _camera.LookAt(FocusTarget());
+    }
+
+    private void PlayHeadHobMotion(Vector3 motion)
+    {
+        _camera.localPosition += motion;
+    }
+
+    private void CheckHeadHobMotion()
+    {
+        float speed = new Vector3(cc.velocity.x, 0, cc.velocity.z).magnitude;
+
+        if (speed < headHobToggleSpeed) return;
+        if (!cc.isGrounded) return;
+
+        PlayHeadHobMotion(FootStepMotion());
+    }
+
+    private Vector3 FootStepMotion()
+    {
+        Vector3 pos = Vector3.zero;
+        pos.y += Mathf.Sin(Time.time * headHobFrequency) * headHobAmplitude;
+        pos.x += Mathf.Cos(Time.time * headHobFrequency / 2) * headHobAmplitude * 2;
+        return pos;
+    }
+
+    private void ResetHeadPosition()
+    {
+        if (_camera.localPosition == headStartPos) return;
+        _camera.localPosition = Vector3.Lerp(_camera.localPosition, headStartPos, Time.deltaTime);
+    }
+
+    private Vector3 FocusTarget()
+    {
+        Vector3 pos = new Vector3(transform.position.x, transform.position.y + cameraHolder.localPosition.y, transform.position.z);
+        pos += cameraHolder.forward * 15f;
+        return pos;
+    }
+
+    private void CameraMove()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime;
+
+        MouseAngleY -= mouseY * mouseSensivity.y;
+        MouseAngleY = Mathf.Clamp(MouseAngleY, -90, 80);
+        cameraHolder.localRotation = Quaternion.Euler(MouseAngleY, 0, 0);
+
+        transform.Rotate(new Vector3(0, mouseX * mouseSensivity.x, 0));
+    }
+
+    private void Move()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+
+        var dir = new Vector3(h, 0, v);
+        dir = transform.TransformDirection(dir).normalized;
+        dir *= moveSpeed;
+
+        if (cc.isGrounded) YVelocity = 0;
+        else YVelocity += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+
+        //if(Input.GetKeyDown(KeyCode.Space))
+        dir.y = YVelocity;
+
+        cc.Move(dir * Time.deltaTime);
+    }
+}
