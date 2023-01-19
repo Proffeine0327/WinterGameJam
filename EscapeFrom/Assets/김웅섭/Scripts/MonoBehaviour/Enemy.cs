0311a6fc4c768a242863e33709c11a2013c4deb8
proffeine0327 <proffeine0327@gmail.com>
2023-01-19 23:03:08 +0900

    Low scaling, Emeny appearance test

---
+++ EscapeFrom/Assets/김웅섭/Scripts/MonoBehaviour/Enemy.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private void Update() 
+    {
+        var pos = Player.player.transform.position;
+        pos.y = transform.position.y;
+        transform.LookAt(pos);    
+    }
+}
--- EscapeFrom/Assets/김웅섭/Scripts/MonoBehaviour/Player.cs
+++ EscapeFrom/Assets/김웅섭/Scripts/MonoBehaviour/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    private static Player player;
+    public static Player player;
 
     [Header("Move")]
     [SerializeField] private float moveSpeed;
