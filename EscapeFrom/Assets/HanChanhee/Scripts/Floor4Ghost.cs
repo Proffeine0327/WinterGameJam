b26a5c632433e46d44dbc4a855ef5d6bf0abbe4f
PotatoCat0325 <hanc02095@gmail.com>
2023-02-12 23:41:57 +0900

    Add Floor4System

--- EscapeFrom/Assets/HanChanhee/Scripts/Floor4Ghost.cs
+++ EscapeFrom/Assets/HanChanhee/Scripts/Floor4Ghost.cs
@@ -11,12 +11,85 @@ public class Floor4Ghost : MonoBehaviour
 
     public Material changeMonitor;
 
+    public Material interactMointor;
 
+    public bool isEnable = false;
     
+
+    public void MonitorOn(Transform monitor)
+    {
+        MeshRenderer meshRenderer = monitor.GetComponent<MeshRenderer>();
+        meshRenderer.material = changeMonitor;
+        StartCoroutine(ComputerOn());
+        
+    }
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        MeshRenderer meshRenderer = moniters[Random.Range(0, moniters.Length)].GetChild(3).GetComponent<MeshRenderer>();
+        meshRenderer.material = interactMointor;
+        InteractMonitor interM = meshRenderer.transform.gameObject.AddComponent<InteractMonitor>();
+        interM.floor4Ghost = this;
+
+
         
+    }
+
+
+    IEnumerator ComputerOn()
+    {
+        List<MeshRenderer> renderers = new List<MeshRenderer>();
+        for(int i = 0; i < moniters.Length; i++)
+        {
+            renderers.Add(moniters[i].GetChild(3).GetComponent<MeshRenderer>());
+        }
+        int random = Random.Range(0, renderers.Count);
+        renderers[random].material = changeMonitor;
+        renderers.RemoveAt(random);
+        yield return new WaitForSeconds(2f);
+        for(int i = 0; i < 4; i++)
+        {
+            random = Random.Range(0, renderers.Count);
+            renderers[random].material = changeMonitor;
+            renderers.RemoveAt(random);
+            yield return new WaitForSeconds(2f);
+        }
+        yield return new WaitForSeconds(1f);
+        for (int i = 0; i < 10; i++)
+        {
+            random = Random.Range(0, renderers.Count);
+            renderers[random].material = changeMonitor;
+            renderers.RemoveAt(random);
+            yield return new WaitForSeconds(1f);
+        }
+        yield return new WaitForSeconds(1f);
+        for (int i = 0; i < 20; i++)
+        {
+            random = Random.Range(0, renderers.Count);
+            if(renderers[random] == null)
+            {
+                break;
+            }
+            renderers[random].material = changeMonitor;
+            renderers.RemoveAt(random);
+            yield return new WaitForSeconds(0.5f);
+        }
+        yield return new WaitForSeconds(3f);
+        GameObject[] ghosts = new GameObject[4];
+        for(int i = 0; i < 4; i++)
+        {
+            ghosts[i] = Instantiate(ghost, moniters[Random.Range(0, moniters.Length)].position, Quaternion.identity);
+            yield return new WaitForSeconds(5f);
+        }
+        yield return new WaitForSeconds(180f);
+        foreach(GameObject g in ghosts)
+        {
+            g.SetActive(false);
+        }
+
     }
 
     // Update is called once per frame
